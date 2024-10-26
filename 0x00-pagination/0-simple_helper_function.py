caa5355866eb24cb7613874d2c2bf9a4502d2fbf
Amira Walid <amirawaleed012@gmail.com>
2024-10-26 15:33:12 +0300

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
+"""
+This module provides a helper function, `index_range`, to calculate the start
+and end indices for paginated data based on a given page number and page size.
+"""
+
+
 def index_range(page, page_size):
+    """
+    Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for pagination.
+
+    Parameters:
+    - page (int): The page number (1-indexed).
+    - page_size (int): The number of items per page.
+
+    Returns:
+    - tuple: A tuple containing the start index and end index.
+    """
     return ((page-1) * page_size, page * page_size)
